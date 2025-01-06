b19506b0822d8c571432ac974fdba8eaab98526f
BassamKhafagy <bassam.nasser@megatrust.co>
2025-01-06 08:46:05 +0200

    initial commit

---
+++ toastlibrary/src/main/java/com/toastMe/ToastUtil.kt
@@ -0,0 +1,19 @@
+package com.toastMe
+
+import android.content.Context
+import android.widget.Toast
+
+object ToastUtil {
+    fun showToast(
+        context: Context,
+        message: String,
+        isLong: Boolean = false
+    ) {
+        val toast = Toast.makeText(
+            context,
+            message,
+            if (isLong) Toast.LENGTH_LONG else Toast.LENGTH_SHORT
+        )
+        toast.show()
+    }
+}
